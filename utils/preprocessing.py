ed85e26bb9bf087acf02ed9a8cf770faef56b7b3
Tisha <115225061+ShravyaMalogi@users.noreply.github.com>
2025-08-20 02:43:01 +0530

    Fifth commit

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,4 +1,3 @@
-# utils/preprocessing.py
 import cv2
 import numpy as np
 
@@ -7,7 +6,6 @@ def preprocess_face(face_img):
     Preprocess face image for UTKFace grayscale model
     Returns (1, 128, 128, 1)
     """
-    # Convert to grayscale if it's not already
     if len(face_img.shape) == 3 and face_img.shape[2] == 3:
         gray_face = cv2.cvtColor(face_img, cv2.COLOR_BGR2GRAY)
     elif len(face_img.shape) == 2:
@@ -17,6 +15,6 @@ def preprocess_face(face_img):
 
     resized_face = cv2.resize(gray_face, (128, 128))
     normalized_face = resized_face.astype('float32') / 255.0
-    normalized_face = np.expand_dims(normalized_face, axis=-1)  # (128,128,1)
-    normalized_face = np.expand_dims(normalized_face, axis=0)   # (1,128,128,1)
+    normalized_face = np.expand_dims(normalized_face, axis=-1)  
+    normalized_face = np.expand_dims(normalized_face, axis=0)   
     return normalized_face
